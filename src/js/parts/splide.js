0c3edd93e49c534c4d0bfdba701aa02eb5e716c2
Uzurch1k <galcenkovlad7@gmail.com>
2024-11-07 17:17:41 +0200

    add projmore

--- src/js/parts/splide.js
+++ src/js/parts/splide.js
@@ -1,13 +1,10 @@
 import { initSlider } from './splidecust';
 
-const functionSplide = document.querySelector('.function');
+const functionSplide = document.querySelector('.projmore');
 if (functionSplide) {
   initSlider(functionSplide, {
-    perPage: 2,
-    breakpoints: {
-      960: {},
-      500: {},
-    },
+    perPage: 1,
+    gap: '1rem',
   });
 }
 
